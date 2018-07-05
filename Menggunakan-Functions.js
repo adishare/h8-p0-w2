4c057b932621ff361c66f81549d845c78c15a55e
fathulqorib <adie.share@gmail.com>
2018-07-05 14:50:56 +0700

    add tugas kamis functions js dan SC codeacademy

---
+++ Menggunakan-Functions.js
@@ -0,0 +1,38 @@
+//Mengenal Panggunaan Function
+//1. ------------------------------------------------------------
+
+function shountOut() {
+  return 'Halo Function!';
+}
+
+console.log(shountOut());
+
+//2. ------------------------------------------------------------
+
+function calculateMultiply(num1, num2) {
+  return num1 * num2;
+}
+
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1, num2);
+
+console.log(hasilPerkalian);
+
+//3. ------------------------------------------------------------
+
+function prosessSentence(name, age, address, hobby) {
+  return `Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}!`;
+}
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentece = prosessSentence(name,age,address,hobby);
+
+console.log(fullSentece);
+
+//
