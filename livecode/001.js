b655cd3aeeb39af034e6e0f5ed7218814a0f1693
fathulqorib <adie.share@gmail.com>
2018-08-02 15:31:23 +0700

    arrange foled file

---
+++ livecode/001.js
@@ -0,0 +1,53 @@
+// Competencies: Pseudocode
+
+// Buatlah Pseudocode untuk kasus berikut:
+// Sebuah perahu layar sedang melaju dengan kecepatan 30km/jam
+// Perahu tersebut dipengaruhi oleh kecepatan ombak yang berlawanan dengan arah lain
+
+// - Jika ombak dibawah 1 meter maka kecepatan tetap.
+// - Jika ombak setinggi 1 meter maka kecepatan akan berkurang sebesar 7km/jam
+// - Jika ombak setinggi 2 meter maka kecepatan akan berkurang sebesar 15km/jam
+// - Jika ombak lebih tinggi daripada 2 meter maka perahu akan terbalik, sehingga kecepatan menjadi 0km/jam
+
+// Tampilkan kecepatan perahu jika di depan ada ombak setinggi x meter
+
+// //JAWAB
+
+// STORE kecepatanPerahu with 30
+// STORE tinggiOmbak with any value
+
+// IF tinggiOmbak less than 1
+//     kecepatanPerahu equals to kecepatanPerahu
+// ELSE IF tinggiOmbak equals to 1
+//     kecepatanPerahu - 7
+// ELSE IF tinggiOmbak equals to 2
+//     kecepatanPerahu - 15
+// ELSE
+//     kecepatanPerahu equals to 0
+
+
+
+// DISPLAY kecepatanPerahu
+
+// //perbaikan
+
+
+// STORE kecepatanPerahu with 30
+// STORE tinggiOmbak with any value
+// STORE kecepatanOmbak with any value
+// STORE result with 0
+
+// IF tinggiOmbak less than 1
+//     kecepatanOmbak equals to 0
+// ELSE IF tinggiOmbak equals to 1
+//     kecepatanOmbak equals to 7
+// ELSE IF tinggiOmbak equals to 2
+//     kecepatanOmbak equals to 15
+// ELSE
+//     kecepatanOmbak equals to 0
+
+// CALCULATE kecepatanPerahu - kecepatanOmbak
+// SET result with value of calculation result 
+
+
+// DISPLAY result
